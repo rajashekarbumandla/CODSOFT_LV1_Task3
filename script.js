1cca555c52e9adcc511dda030d60b665a22f26f6
Rajashekar Bumandla <bumandlarajashekar@gmail.com>
2024-09-07 12:55:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+} 
+ 
+function clearElement(){ 
+    const display=document.getElementById('display'); 
+    display.value=display.value.toString().slice(0,-1); 
+
+}
